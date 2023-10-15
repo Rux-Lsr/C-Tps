d2c3154e48e575cb2d6821eb230b47e9a1939c04
Rux-Lsr <russelsonwa1@gmail.com>
2023-10-15 20:15:52 +0100

    Code source de Base

---
+++ Salaire.cs
@@ -0,0 +1,20 @@
+namespace AppSalaire{
+     public enum Poste : int
+    {
+        Comptable = 0,
+        Secretaire = 1,
+        Informatitien = 2,
+        AutrePoste = 3
+    }
+    public class Salaire
+    {
+        public string NomEmploye ;
+        public string SexeEmploye;
+        public DateTime AnneRecrutement;
+        public Poste PosteEmploye;
+        public int SalaireEmploye ;
+        public string NomStructure 
+
+       
+    }
+}
